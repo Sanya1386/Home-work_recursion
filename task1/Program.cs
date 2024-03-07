08afb7a5b1c0605929d5daa02e2039b2d48baecd
Sanya <Sanya-dmb@yandex.ru>
2024-03-08 00:32:51 +0700

    task1,2

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,3 +1,52 @@
 ﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
 // в промежутке от M до N. Использовать рекурсию, не использовать циклы.
 
+Console.Write("Введи N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+
+void ShowNaturalNumber(int M, int N)
+{
+    int mindigit;
+    int maxdigit;
+    if (M < N)
+    {
+        mindigit = M;
+        maxdigit = N;
+    }
+    else
+    {
+        mindigit = N;
+        maxdigit = M;
+    }
+    if(mindigit != maxdigit)
+    {
+        Console.Write(mindigit + ", ");
+        ShowNaturalNumber(mindigit + 1, maxdigit);
+        //Console.Write(mindigit + ", ");
+    }
+    else {
+        Console.Write(maxdigit);
+    }
+}
+
+ShowNaturalNumber(M, N);
+// void Main()
+// {
+// Console.Write("Введи N: ");
+// int N = Convert.ToInt16(Console.ReadLine());
+// Console.Write("Введи M: ");
+// int M = Convert.ToInt16(Console.ReadLine());
+// Console.WriteLine($"N={N}, M={M}");
+// if (M>N)
+// for (int i = N; i <= M; i++)
+//     Console.Write($" {i}");
+// else
+//     for (int i = M; i <= N; i++)
+//         Console.Write($" {i}");
+// }
+
+
+// Main();
--- task2/Program.cs
+++ task2/Program.cs
@@ -1,4 +1,14 @@
-﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии.
-//  Даны два неотрицательных числа m и n.
-
-
+﻿void Main()
+{
+    int result = Ackermann (2, 5);
+    Console.WriteLine ("Результат: " + result); //когда n=2 и m=5, результат будет равен 13
+ 
+    result = Ackermann (1, 2);
+    Console.WriteLine ("Результат: " + result); //когда n=1 и m=2, результат будет равен 4
+}
+ 
+ static int Ackermann (int n, int m) => (n < 0 || m < 0) 
+                        ? throw new ArgumentOutOfRangeException() 
+                        : (n == 0) ? (m + 1) 
+                        : (m == 0) ? Ackermann (n - 1, 1)
+                        : Ackermann (n - 1, Ackermann (n, m - 1));
